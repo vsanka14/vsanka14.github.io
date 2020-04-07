bddfe9b3f5034f76d4df15e5ce671dd3e5f56c25
svasisht <vasisht.shankar@gmail.com>
2020-04-07 10:15:03 -0700

    fixed popup_img bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     document.getElementById('loader').style.display = 'none';
-    document.getElementsByClassName('popup_img').forEach(element=>{
+    document.getElementsByClassName('popup__img').forEach(element=>{
         element.style.visibility = 'visible';
     });
 });
